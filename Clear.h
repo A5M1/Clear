b7d58c8c905c8343ea2db003ea6a4652860221ff
Beanos <admin@abby0666.xyz>
2024-04-15 12:38:48 -0700

    Add project files.

---
+++ Clear.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <processenv.h>
+#include <WinBase.h>
+#include <consoleapi2.h>
+
+/**
+  Clears the console screen.
+ */
+void clear() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coordScreen = { 0, 0 };
+	DWORD cCharsWritten;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD dwConSize;
+	GetConsoleScreenBufferInfo(hConsole, &csbi);
+	dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+	FillConsoleOutputCharacter(hConsole, (TCHAR)' ', dwConSize, coordScreen, &cCharsWritten);
+	FillConsoleOutputAttribute(hConsole, csbi.wAttributes, dwConSize, coordScreen, &cCharsWritten);
+	SetConsoleCursorPosition(hConsole, coordScreen);
+}
---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include "Clear.h"
+
+
+
+int main() {
+	clear();
+	return 0;
+}
+
